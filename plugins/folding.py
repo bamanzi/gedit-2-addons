1df1cfb1ba613a052d6ad05c81a18dff90fb54c2
bamanzi <none@none>
2012-10-25 21:23:14 +0800

    update plugin 'folding' with this fork: https://github.com/alanhamlett/gedit-folding

--- plugins/folding.py
+++ plugins/folding.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import gtk
 try:
-    import gedit
+	import gedit
 except:
-    import pluma as gedit
+	import pluma as gedit
 
 ui_str = """<ui>
 	<menubar name="MenuBar">
 		<menu name="ToolsMenu" action="Tools">
 			<placeholder name="ToolsOps_2">
-				<menuitem name="Folding" action="FoldingPy"/>
+				<menuitem name="ToggleFold" action="ToggleFold"/>
+			</placeholder>
+			<placeholder name="ToolsOps_2">
+				<menuitem name="FoldDeepest" action="FoldDeepest"/>
+			</placeholder>
+			<placeholder name="ToolsOps_2">
+				<menuitem name="UnFoldAll" action="UnFoldAll"/>
 			</placeholder>
 		</menu>
 	</menubar>
@@ -28,19 +34,14 @@ class FoldingPyWindowHelper():
 		self._window = None
 		self._plugin = None
 		self._action_group = None
+		#self.fold_off
 
 	def _insert_menu(self):
 		manager = self._window.get_ui_manager()
 		self._action_group = gtk.ActionGroup("FoldingPyPluginActions")
-		self._action_group.add_actions(
-			[
-				(
-					"FoldingPy", None, "Fold",
-					"<Alt>Z", "Fold",
-					self.fold
-				)
-			],
-		)
+		self._action_group.add_actions([("ToggleFold", None, _("Fold/Unfold"), "<Alt>Z", _("Fold/Unfold"), lambda a: self.fold())])
+		self._action_group.add_actions([("FoldDeepest", None, _("Fold Deepest"), "<Alt>X", _("Fold Deepest"), lambda a: self.fold_deepest())])
+		self._action_group.add_actions([("UnFoldAll", None, _("Un-Fold All"), "<Shift><Alt>X", _("Un-Fold All"), lambda a: self.fold_off())])
 		manager.insert_action_group(self._action_group, -1)
 		self._ui_id = manager.add_ui_from_string(ui_str)
 
@@ -55,8 +56,6 @@ class FoldingPyWindowHelper():
 		self.doc=self._window.get_active_document()
 		if self.doc:
 			self.view=self._window.get_active_view()
-			self.view.connect('key-press-event', self.fold_off)
-			
 			table=self.doc.get_tag_table()
 			self.fld=table.lookup('fld')
 			if self.fld==None:
@@ -71,18 +70,50 @@ class FoldingPyWindowHelper():
 		sps=sps[:i]
 		return sps.count(' ')+sps.count('\t')*self.view.get_tab_width()
 
-	def fold_off(self,widget,e=None):
-		if e.hardware_keycode==53 and self.keycode==64:
-			s,e=self.doc.get_bounds()
-			self.doc.remove_tag(self.fld,s,e)
-			self.doc.remove_tag(self.inv,s,e)
-			self.keycode=53
-			print "SimpleFolding plugin: remove all fold"
-		else:
-			self.keycode=e.hardware_keycode
+	def fold_deepest(self,action=None):
+		deepest = 0
+		lines = list()
+		s = self.doc.get_iter_at_line(0)
+		e = s.copy()
+		sg = 0
+		eg = 0
+		while s.forward_visible_line():
+			if s.get_char()!="\n":
+				e.set_line(s.get_line())
+				e.forward_to_line_end()
+				text = s.get_text(e)
+				if text.strip()!="":
+					indent = self.detect_sps(text)
+					if indent:
+						if indent > deepest:
+							deepest = indent
+							lines = list()
+							sg = s.get_line()
+							eg = s.get_line()
+						elif indent < deepest and eg:
+							lines.append((sg-1,eg))
+							eg = 0
+						elif indent == deepest:
+							if not eg:
+								sg = s.get_line()
+							eg = s.get_line()
+		if eg:
+			lines.append((sg-1,eg))
+		for (sg, eg) in lines:
+			s.set_line(sg)
+			e.set_line(eg)
+			self.fold(None, s, e)
+		print "SimpleFolding plugin: create %d folds by %d indent" % (len(lines), deepest)
 
-	def fold(self, action):
-		a=self.doc.get_iter_at_mark(self.doc.get_insert())
+	def fold_off(self,action=None):
+		s,e=self.doc.get_bounds()
+		self.doc.remove_tag(self.fld,s,e)
+		self.doc.remove_tag(self.inv,s,e)
+		print "SimpleFolding plugin: remove all fold"
+
+	def fold(self, action=None, a=None, c=None):
+		if a==None:
+			a=self.doc.get_iter_at_mark(self.doc.get_insert())
 		if a.has_tag(self.fld):
 			try:
 				a.set_line_offset(0)
@@ -96,57 +127,59 @@ class FoldingPyWindowHelper():
 			except:
 				pass
 
-		elif len(self.doc.get_selection_bounds())==2:
-			a,c=self.doc.get_selection_bounds()
+		elif (a!=None and c!=None) or len(self.doc.get_selection_bounds())==2:
+			if c==None:
+				a,c=self.doc.get_selection_bounds()
+			if a.get_line() == c.get_line():
+				return
 			b=a.copy()
 			a.set_line_offset(0)
 			b.forward_line()
 			c.forward_line()
 			self.doc.apply_tag(self.fld,a,b)
+			# TODO: Don't remove already folded tags and keep track of nested tags
 			self.doc.remove_tag(self.fld,b,c)
 			self.doc.remove_tag(self.inv,b,c)
 			self.doc.apply_tag(self.inv,b,c)
 			print "SimpleFolding plugin: create fold by selection"
 
 		else:
-			s=a.copy()
-			s.set_line_offset(0)
-			line = s.get_line()
-			e=s.copy()
-			e.forward_line()
-			t=s.get_text(e)
-			if t.strip()!="":
-				main_indent = self.detect_sps(s.get_text(e))
-				ns=s.copy()
-				fin=ns.copy()
+			a.set_line_offset(0)
+			line = a.get_line()
+			sfold = a.copy()
+			sfold.forward_line()
+			text = a.get_text(sfold)
+			if text.strip()!="":
+				main_indent = self.detect_sps(text)
+				fin = a.copy()
+				e = a.copy()
 				while 1==1:
-					if ns.forward_line():
-						ne=ns.copy()
-						if ne.get_char()=="\n":
+					if e.forward_line():
+						if e.get_char()=="\n":
 							continue
+						ne=e.copy()
 						ne.forward_to_line_end()
-						text=ns.get_text(ne)
+						text=e.get_text(ne)
 						if text.strip()=="":
 							continue
 						child_indent=self.detect_sps(text)
 						if child_indent <= main_indent:
 							break
 						else:
-							line=ns.get_line()
-						fin=ns.copy()
+							line=e.get_line()
 						fin.set_line(line)
 						fin.forward_line()
 					else:
-						fin=ne.copy()
 						fin.forward_to_end()
 						line=fin.get_line()
 						break
 				
-				if s.get_line()<line:
-					self.doc.apply_tag(self.fld,s,e)
-					self.doc.remove_tag(self.fld,e,fin)
-					self.doc.remove_tag(self.inv,e,fin)
-					self.doc.apply_tag(self.inv,e,fin)
+				if a.get_line()<line:
+					self.doc.apply_tag(self.fld,a,sfold)
+					# TODO: Don't remove already folded tags and keep track of nested tags
+					self.doc.remove_tag(self.fld,sfold,fin)
+					self.doc.remove_tag(self.inv,sfold,fin)
+					self.doc.apply_tag(self.inv,sfold,fin)
 					print "SimpleFolding plugin: create fold by indent"
 
 

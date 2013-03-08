292b90fe7fb78cdfbaa2dfc9a4452ade9002aa48
bamanzi <none@none>
2013-03-08 21:29:47 +0800

    add plugin pylint

---
+++ plugins/pylint/__init__.py
@@ -0,0 +1,25 @@
+try:
+    import gedit
+except:
+    import pluma as gedit
+
+from gedit_pylint import PylintInstance
+
+class PylintPlugin (gedit.Plugin):
+
+    def __init__(self):
+        self._instances = {}
+        
+        super(PylintPlugin, self).__init__ ()
+
+    def activate(self, window):
+        self._instances[window] = PylintInstance (self, window)
+
+    def deactivate(self, window):
+        if self._instances.has_key(window):
+            self._instances[window].deactivate()
+            del self._instances[window]
+
+    def update_ui(self, window):
+        if self._instances.has_key(window):
+            self._instances[window].update_ui()

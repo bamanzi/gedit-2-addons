20d72660979b4845945a8f5381cbd1237ab4854e
bamanzi <none@none>
2013-03-08 22:27:22 +0800

    make plugin switcher support mate-text-editor

--- plugins/switcher.py
+++ plugins/switcher.py
@@ -18,7 +18,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-import gedit
+try:
+	import gedit
+except:
+	import pluma as gedit
+
 import gtk
 import gobject
 
